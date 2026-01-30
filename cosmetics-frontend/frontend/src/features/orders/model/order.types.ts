051c432ec15a586dd276169500fea054cccc91e9
Yaroslav-662 <paroslav662@gmail.com>
2026-01-31 00:02:27 +0200

    Update order.types.ts

--- cosmetics-frontend/frontend/src/features/orders/model/order.types.ts
+++ cosmetics-frontend/frontend/src/features/orders/model/order.types.ts
@@ -1,4 +1,6 @@
 // src/features/orders/model/order.types.ts
+export type UserRole = "user" | "admin";
+
 export type OrderStatus =
   | "pending"
   | "paid"
@@ -12,13 +14,24 @@ export type OrderItem = {
   quantity: number;
 };
 
+export type OrderUser = {
+  _id: string;
+  name?: string;
+  email?: string;
+  role?: UserRole;
+};
+
 export type Order = {
   _id: string;
+  user?: OrderUser | string;
+
   items: OrderItem[];
   address?: string;
   paymentMethod?: string;
+
   status: OrderStatus;
   total: number;
+
   createdAt: string;
   updatedAt?: string;
 };
