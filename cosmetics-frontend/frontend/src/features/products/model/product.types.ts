2f28d78d70a9f1550973705e57a519436e3e6405
Yaroslav-662 <paroslav662@gmail.com>
2026-02-05 20:46:19 +0200

    Update product.types.ts

--- cosmetics-frontend/frontend/src/features/products/model/product.types.ts
+++ cosmetics-frontend/frontend/src/features/products/model/product.types.ts
@@ -1,23 +1,25 @@
 // src/features/products/model/product.types.ts
 
-export interface Product {
+export type Product = {
   _id: string;
   name: string;
   price: number;
   description: string;
-  category: string;
-  images?: string[];
+  category: string; // або об'єкт — якщо в тебе populated
   stock: number;
+  images: string[];
   createdAt?: string;
   updatedAt?: string;
-}
+};
 
-export interface CreateProductDto {
+export type CreateProductDto = {
   name: string;
   price: number;
   description: string;
   category: string;
   stock: number;
-}
+  images: string[];
+};
 
-export interface UpdateProductDto extends Partial<CreateProductDto> {}
+export type UpdateProductDto = Partial<CreateProductDto>;
+
