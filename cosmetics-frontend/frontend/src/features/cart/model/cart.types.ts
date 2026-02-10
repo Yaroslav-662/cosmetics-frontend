73611a29f33fe44837badf402b7149e07eba2496
Yaroslav-662 <paroslav662@gmail.com>
2026-02-10 13:33:03 +0200

    Update cart.types.ts

--- cosmetics-frontend/frontend/src/features/cart/model/cart.types.ts
+++ cosmetics-frontend/frontend/src/features/cart/model/cart.types.ts
@@ -1,27 +1,16 @@
 // src/features/cart/model/cart.types.ts
 
-import type { Product } from "@/features/products/model/product.types";
+export interface CartItem {
+  id: string;
+  title: string;
+  price: number;
+  image: string; // ПОВНИЙ URL
+  qty: number;
+}
 
-export interface CartProduct {
+export interface AddToCartPayload {
   id: string;
   title: string;
   price: number;
   image: string;
 }
-
-export interface CartItem extends CartProduct {
-  quantity: number;
-}
-
-export interface CartState {
-  items: CartItem[];
-  addToCart: (product: CartProduct) => void;
-  removeFromCart: (id: string) => void;
-  increaseQty: (id: string) => void;
-  decreaseQty: (id: string) => void;
-  clearCart: () => void;
-  totalPrice: () => number;
-  totalItems: () => number;
-}
-
-
